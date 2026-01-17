36b5608f85c80ae9ab3dd3ac457c29062bb63ad8
David Rotermund <davrot@uni-bremen.de>
2026-01-17 20:32:42 +0100

    Clean up compliance commands and add build scripts
    Removed compliance commands and added build scripts.

--- build_mattermost_docker/extract_code.sh
+++ build_mattermost_docker/extract_code.sh
@@ -16,14 +16,14 @@ echo "package enterprise" > mattersource/server/enterprise/enterprise.go
 echo "package metrics" > mattersource/server/enterprise/metrics/metrics.go
 echo "package shared" > mattersource/server/enterprise/message_export/shared/shared.go
 
-rm -rf mattersource/server/cmd/mmctl/commands/compliance
-rm -f mattersource/server/cmd/mmctl/commands/compliance_export.go
-
 cp -rvf ./enterprise_replace/* mattersource/
 cp -f logo/* mattersource/webapp/channels/src/images
 cp -f favicon/* mattersource/webapp/channels/src/images/favicon
 # cp -f logo_dark_blue_svg.tsx mattersource/webapp/channels/src/components/common/svg_images_components/logo_dark_blue_svg.tsx
 # cp -f mattermost_logo.tsx mattersource/webapp/channels/src/components/widgets/icons/mattermost_logo.tsx
+
+rm -rf mattersource/server/cmd/mmctl/commands/compliance
+rm -f mattersource/server/cmd/mmctl/commands/compliance_export.go
 
 cp build_without_docker.sh mattersource
 cp build_with_docker.sh mattersource
