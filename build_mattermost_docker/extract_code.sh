579f3223d7ca116a96fc27f4e997bcf210c5fe2f
David Rotermund <davrot@uni-bremen.de>
2026-01-17 19:11:35 +0100

    Comment out logo and favicon copy commands
    Comment out favicon and logo file copy commands in extract_code.sh

--- build_mattermost_docker/extract_code.sh
+++ build_mattermost_docker/extract_code.sh
@@ -21,9 +21,9 @@ rm -f mattersource/server/cmd/mmctl/commands/compliance_export.go
 
 cp -rvf ./enterprise_replace/* mattersource/
 cp -f logo/* mattersource/webapp/channels/src/images
-cp -f favicon/* mattersource/webapp/channels/src/images/favicon
-cp -f logo_dark_blue_svg.tsx mattersource/webapp/channels/src/components/common/svg_images_components/logo_dark_blue_svg.tsx
-cp -f mattermost_logo.tsx mattersource/webapp/channels/src/components/widgets/icons/mattermost_logo.tsx
+# cp -f favicon/* mattersource/webapp/channels/src/images/favicon
+# cp -f logo_dark_blue_svg.tsx mattersource/webapp/channels/src/components/common/svg_images_components/logo_dark_blue_svg.tsx
+# cp -f mattermost_logo.tsx mattersource/webapp/channels/src/components/widgets/icons/mattermost_logo.tsx
 
 cp build_without_docker.sh mattersource
 cp build_with_docker.sh mattersource
