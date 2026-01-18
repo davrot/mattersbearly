4c7d3ac5d993af42a312724062abba26745b3fa5
David Rotermund <davrot@uni-bremen.de>
2026-01-18 03:06:44 +0100

    Add logo_gray_svg.tsx to extraction script

--- build_mattermost_docker/extract_code.sh
+++ build_mattermost_docker/extract_code.sh
@@ -20,6 +20,7 @@ cp -rvf ./enterprise_replace/* mattersource/
 cp -f logo/* mattersource/webapp/channels/src/images
 cp -f favicon/* mattersource/webapp/channels/src/images/favicon
 cp -f logo_dark_blue_svg.tsx mattersource/webapp/channels/src/components/common/svg_images_components/logo_dark_blue_svg.tsx
+cp -f logo_gray_svg.tsx mattersource/webapp/channels/src/components/common/svg_images_components/logo_gray_svg.tsx
 cp -f mattermost_logo.tsx mattersource/webapp/channels/src/components/widgets/icons/mattermost_logo.tsx
 
 rm -rf mattersource/server/cmd/mmctl/commands/compliance
