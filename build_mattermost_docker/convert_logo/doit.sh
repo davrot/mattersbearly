4bc83440de5b6634d2dd488d526b82908bac145a
David Rotermund <davrot@uni-bremen.de>
2026-01-18 03:09:04 +0100

    Add files via upload

--- build_mattermost_docker/convert_logo/doit.sh
+++ build_mattermost_docker/convert_logo/doit.sh
@@ -1,3 +1,5 @@
 npm install -g @svgr/cli
 svgr --typescript logo_dark_blue_svg.svg > ../logo_dark_blue_svg.tsx
+svgr --typescript logo_gray_svg.svg > ../logo_gray_svg.tsx
 svgr --typescript mattermost_logo.svg > ../mattermost_logo.tsx
+
